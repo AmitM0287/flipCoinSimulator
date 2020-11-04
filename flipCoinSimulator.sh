426284b0f462125312ddbb9b70b295fc0d9a5693
AmitM0287 <amitmanna0287@gmail.com>
2020-11-04 18:44:06 +0530

    either of them had own 21 times by how much or tie

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -15,9 +15,35 @@ flipCoin()
         fi
 }
 
-for (( i=0; i<10; i++ ))
+for (( i=0; i<70; i++ ))
 do
 	flipCoin
+
+	if (( $H==21 ))
+	then
+		d=`expr $H - $T`
+		if (( $d<0 ))
+		then
+			d=`expr $d \* -1`
+		fi
+
+		echo "Head Win! By $d"
+		break
+
+	elif (( $T==21 ))
+	then
+		d=`expr $T - $H`
+                if (( $d<0 ))
+                then
+                        d=`expr $d \* -1`
+                fi
+
+		echo "Tail Win! By $d"
+		break
+
+	elif (( $H==21 && $T==21 ))
+	then
+		echo "It's a Tie!"
+		break
+	fi
 done
-echo "Head Win $H times"
-echo "Tail Win $T Times"
